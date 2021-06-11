127e606316dc7c7cd58bbedc186c66a1d039a0e6
Martina-St <martina.stegmaier@gmail.com>
2021-06-11 11:38:45 +0200

    adding jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello and welcome! Have a look around!");
+
+function myFunction() {
+   var x = document.getElementById('myNavtoggle');
+   if (x.className === 'navtoggle') {
+     x.className += ' responsive';
+   } else {
+     x.className = 'navtoggle';
+   }
+ }
