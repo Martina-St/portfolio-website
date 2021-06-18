ed26f6665ff529fdb4393207aa25d103e4e676e1
Martina-St <martina.stegmaier@gmail.com>
2021-06-18 09:43:27 +0200

    eliminating errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,10 @@
-console.log("Hello and welcome! Have a look around!");
+console.log('Hello and welcome! Have a look around!')
 
-
-function menuToggle() {
-   var x = document.getElementById('myNavtoggle');
-   if (x.className === 'navtoggle') {
-     x.className += ' responsive';
-   } else {
-     x.className = 'navtoggle';
-   }
- }
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle' && $(window).width() < 640) {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
